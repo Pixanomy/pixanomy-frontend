f90202101a9c297b672e9d29232370d60b5b17e4
Sebastian hilton <bastianh86@outlook.com>
2023-04-08 17:21:54 -0700

    updated errors with build

--- .nuxt/types/layouts.d.ts
+++ .nuxt/types/layouts.d.ts
@@ -1,5 +1,5 @@
 import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default" | "no-live"
+export type LayoutKey = "default"
 declare module "C:/Users/Basti/OneDrive/Documents/My Websites/Handmade Sites/Javascript-Projects/Nuxt-Projects/AlternateCMS-Framework/PixanomyCompany-Sites/pixanomy-frontend/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
