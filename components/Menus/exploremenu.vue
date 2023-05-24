93e605a3f38b1a1b5547d9d9f1de88712a9fd151
Sebastian hilton <bastianh86@outlook.com>
2023-05-23 22:39:14 -0700

    updated exploremenu

--- components/Menus/exploremenu.vue
+++ components/Menus/exploremenu.vue
@@ -2,7 +2,6 @@
     <v-card>
         <v-tabs bg-color="transparent" align-tabs="center">
             <v-tab><a href="/explore/">Explore</a></v-tab>
-            <v-tab><a href="/categories/visuals">Videos</a></v-tab>
             <v-tab><a href="/explore/leaderboard">Leaderboard</a></v-tab>
             <v-tab><a href="/explore/collections">Collections</a></v-tab>
             <v-tab><a href="/categories/market">Themes</a></v-tab>
